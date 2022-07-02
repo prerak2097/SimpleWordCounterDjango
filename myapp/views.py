669e964a4f730ca31a2d4e12b76ab9c3cf471ad6
Prerak Patel <prerak2000@gmail.com>
2022-07-02 13:37:07 -0400

    using secure URL

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,6 @@ def index(request):
     return render(request, 'index.html')
 
 def counter(request):
-    text = request.GET['text']
+    text = request.POST['text']
     word_count = len(text.split())
     return render(request, 'counter.html', {'amount': word_count})
